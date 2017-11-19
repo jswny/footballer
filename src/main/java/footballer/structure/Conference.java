77e19fc45b9bb29366199ab595d39a5ed36948fa
Joe Sweeney <joe@j1.io>
2017-11-18 19:00:31 -0500

    Add docs to conference class

--- src/main/java/footballer/structure/Conference.java
+++ src/main/java/footballer/structure/Conference.java
@@ -3,6 +3,9 @@ package footballer.structure;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Defines an NFL conference.
+ */
 public class Conference {
     public final String name;
     private List<Division> divisions = new ArrayList<>();
@@ -11,6 +14,11 @@ public class Conference {
         name = n;
     }
 
+    /**
+     * Adds a {@link Division} to this conference.
+     * @param divisionName the name of the {@link Division} to be added
+     * @return the newly created division, or {@code null} if the {@code divisionName} already exists in this conference
+     */
     public Division addDivision(String divisionName) {
         if (getDivision(divisionName) != null) return null;
         Division div = new Division(divisionName);
@@ -18,12 +26,23 @@ public class Conference {
         return div;
     }
 
+    /**
+     * Adds a {@link Team} to a {@link Division} in this conference.
+     * @param divisionName the name of the {@link Division} to add the team to
+     * @param teamName the name of the {@link Team} to add
+     * @return the newly added team, or {@code null} if the specified {@code divisionName} does not exist in this conference
+     */
     public Team addTeam(String divisionName, String teamName) {
         Division div = getDivision(divisionName);
         if (div == null) return null;
         return div.addTeam(teamName);
     }
 
+    /**
+     * Gets a {@link Division} in this conference.
+     * @param divisionName the name of the {@link Division} to get
+     * @return the {@link Division} with a {@code name} field which matches {@code divisionName}, or {@code null} if no such {@link Division} exists
+     */
     public Division getDivision(String divisionName) {
         for (Division div : divisions) {
             if (div.name.equals(divisionName)) return div;
@@ -31,6 +50,10 @@ public class Conference {
         return null;
     }
 
+    /**
+     * Gets a list of all {@link Team}s in this conference.
+     * @return A {@link List} of all {@link Team}s in {@link Division}s in this conference
+     */
     public List<Team> getTeams() {
         List<Team> teams = new ArrayList<>();
         for (Division div : divisions) {
