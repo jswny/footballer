bdfd8080455f5c176d8b7c5bbe042a56d95640c2
Joe Sweeney <joe@j1.io>
2018-06-25 22:57:05 -0400

    Begin adding division filtering API support

--- src/main/java/footballer/web/Server.java
+++ src/main/java/footballer/web/Server.java
@@ -1,17 +1,12 @@
 package footballer.web;
 
-import footballer.Utils;
 import footballer.parse.Parser;
-import footballer.ranking.RankingSystem;
-import footballer.ranking.system.AdjustedWins;
-import footballer.ranking.system.EvenPlay;
-import footballer.ranking.system.SelfBased;
 import footballer.structure.Season;
-import footballer.structure.Team;
 import java.util.List;
 import static spark.Spark.*;
 import com.google.gson.Gson;
 import footballer.web.data.Dataset;
+import footballer.web.data.DatasetEntry;
 
 /**
  * Defines the entry point for the web component of this application.
@@ -30,54 +25,26 @@ public class Server {
 
         path("/api", () -> {
 
-            // Generate one route for each ranking system
             for (String rS : rankingSystems) {
+                // Generate one default route for each ranking system by week
                 get("/rankings/" + rS + "/:week", (req, res) -> {
-                    return getRankingSystemDataset(rS, Integer.parseInt(req.params("week"))).getEntries();
+                    int week = Integer.parseInt(req.params("week"));
+                    Season season = Parser.parseCurrentStructure(2017, week);
+                    List<DatasetEntry> entries = new Dataset(season, rS, week).getEntries();
+                    return entries;
+                }, gson::toJson);
+
+                // Generate one route for each ranking system by week scoped by division
+                get("/rankings/" + rS + "/:week/:conference/:division", (req, res) -> {
+                    int week = Integer.parseInt(req.params("week"));
+                    String conference = req.params("conference");
+                    String division = req.params("division");
+                    Season season = Parser.parseCurrentStructure(2017, week);
+                    List<DatasetEntry> entries = new Dataset(season, rS, week).filterByDivision(season, conference, division).getEntries();
+                    return entries;
                 }, gson::toJson);
             }
         });
     }
 
-    /**
-     * Creates a {@link Dataset} for a given {@link RankingSystem} up to a given {@link footballer.structure.Week}.
-     * This method throws a {@link RuntimeException} if no {@link RankingSystem} which matches {@code rankingSystemName} can be found.
-     *
-     * @param rankingSystemName the name of the {@link RankingSystem} to create the {@link Dataset} for
-     * @param upToWeek the {@link footballer.structure.Week} maximum number (inclusive) for which the {@link RankingSystem} should be populated
-     * @return the {@link Dataset} created using the {@link RankingSystem} which matches {@code rankingSystemName}, populated through {@code upToWeek}
-     */
-    private static Dataset getRankingSystemDataset(String rankingSystemName, int upToWeek) {
-        Season season = Parser.parseCurrentStructure(2017, upToWeek);
-        String[] teamNames = Utils.espnPreseasonRankings;
-        List<Team> teams = season.getTeams();
-
-        RankingSystem rankingSystem;
-        int maxBaseline;
-
-        switch (rankingSystemName) {
-            case "evenplay":
-                maxBaseline = 16;
-                rankingSystem = new EvenPlay(teams, 1.0);
-                break;
-
-            case "adjustedwins":
-                maxBaseline = 0;
-                rankingSystem = new AdjustedWins(teams);
-                break;
-
-            case "selfbased":
-                maxBaseline = 16;
-                rankingSystem = new SelfBased(teams);
-                break;
-
-            default:
-                throw new RuntimeException("Cannot create dataset for ranking system: " + rankingSystemName + "!");
-        }
-
-        rankingSystem.generateBaselineRanks(teamNames, 0, maxBaseline);
-        rankingSystem.applyGames(season);
-
-        return new Dataset(rankingSystem.getLog());
-    }
 }
--- src/main/java/footballer/web/data/Dataset.java
+++ src/main/java/footballer/web/data/Dataset.java
@@ -1,14 +1,61 @@
 package footballer.web.data;
 
+import footballer.Utils;
+import footballer.ranking.RankingSystem;
 import footballer.ranking.logging.Log;
+import footballer.ranking.system.AdjustedWins;
+import footballer.ranking.system.EvenPlay;
+import footballer.ranking.system.SelfBased;
+import footballer.structure.Season;
 import footballer.structure.Team;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Dataset {
     private List<DatasetEntry> entries = new ArrayList<>();
 
-    public Dataset(Log log) {
+    /**
+     * Creates a {@link Dataset} for a given {@link RankingSystem} up to a given {@link footballer.structure.Week}.
+     * Throws a {@link RuntimeException} if no {@link RankingSystem} which matches {@code rankingSystemName} can be found.
+     *
+     * @param rankingSystemName the name of the {@link RankingSystem} to create the {@link Dataset} for
+     * @param upToWeek the {@link footballer.structure.Week} maximum number (inclusive) for which the {@link RankingSystem} should be populated
+     */
+    public Dataset(Season season, String rankingSystemName, int upToWeek) {
+        String[] teamNames = Utils.espnPreseasonRankings;
+        List<Team> teams = season.getTeams();
+
+        RankingSystem rankingSystem;
+        int maxBaseline;
+
+        switch (rankingSystemName) {
+            case "evenplay":
+                maxBaseline = 16;
+                rankingSystem = new EvenPlay(teams, 1.0);
+                break;
+
+            case "adjustedwins":
+                maxBaseline = 0;
+                rankingSystem = new AdjustedWins(teams);
+                break;
+
+            case "selfbased":
+                maxBaseline = 16;
+                rankingSystem = new SelfBased(teams);
+                break;
+
+            default:
+                throw new RuntimeException("Cannot create dataset for ranking system: " + rankingSystemName + "!");
+        }
+
+        rankingSystem.generateBaselineRanks(teamNames, 0, maxBaseline);
+        rankingSystem.applyGames(season);
+
+        populateFromLog(rankingSystem.getLog());
+    }
+
+    private void populateFromLog(Log log) {
         for (Team team : log.getTeams()) {
             entries.add(new DatasetEntry(team.name, log.getTeamValues(team.name)));
         }
@@ -16,5 +63,23 @@ public class Dataset {
 
     public List<DatasetEntry> getEntries() {
         return entries;
+    }
+
+    /**
+     * Filters a {@link Dataset} by a specified {@link footballer.structure.Division} in a {@link footballer.structure.Conference} in a specific {@link Season}.
+     * @param season the {@link Season} to retrieve the {@link footballer.structure.Division} from
+     * @param conferenceName the name of the {@link footballer.structure.Conference} which contains the specified {@link footballer.structure.Division}
+     * @param divisionName the name of the {@link footballer.structure.Division} to filter based on
+     * @return the filtered {@link Dataset}
+     */
+    public Dataset filterByDivision(Season season, String conferenceName, String divisionName) {
+        List<String> teamNamesInDivision = season
+                                    .getDivision(conferenceName, divisionName)
+                                    .getTeams()
+                                    .stream()
+                                    .map(team -> team.name)
+                                    .collect(Collectors.toList());
+        entries.stream().filter(entry -> teamNamesInDivision.contains(entry));
+        return this;
     }
 }
